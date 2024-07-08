3df130fa3a5d09a3e84665a4d9935987aa4e6d1e
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-08 18:17:06 +0300

    add-slider

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -1,3 +1,33 @@
 export function createSlider() {
-  console.log('test');
+  const images = document.querySelectorAll('.js-slider-img');
+  const sliderLine = document.querySelector('.js-slider-line');
+
+  let count = 0;
+  let width;
+
+  function init() {
+    width = document.querySelector('.js-slider-track').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+
+    images.forEach(item => (item.style.width = width + 'px'));
+    rollSlider();
+  }
+
+  window.addEventListener('resize', init);
+  init();
+
+  document.querySelector('.js-slider-btn-next').addEventListener('click', () => {
+    count += 1;
+    if (count >= images.length) count = 0;
+    rollSlider();
+  });
+  document.querySelector('.js-slider-btn-prev').addEventListener('click', () => {
+    count -= 1;
+    if (count < 0) count = images.length - 1;
+    rollSlider();
+  });
+
+  function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+  }
 }
