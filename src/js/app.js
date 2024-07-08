839f189d5682d29b5ec92418631c5a57b9c17097
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-08 16:49:22 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import * as flsFunctions from './partials/functions.js';
 
+import { createSlider } from './partials/slider.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   flsFunctions.isWebp();
+  createSlider();
 });
---
+++ src/js/partials/slider.js
@@ -0,0 +1,3 @@
+export function createSlider() {
+  console.log('test');
+}
