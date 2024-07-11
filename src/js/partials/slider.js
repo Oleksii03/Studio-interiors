52a2b4c66c50b7e8507f0fd6453915e65a2915b6
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-11 17:06:37 +0300

    add-slider

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -1,34 +1,41 @@
 export function createSlider() {
   const images = document.querySelectorAll('.js-slider-img');
   const sliderLine = document.querySelector('.js-slider-line');
+  const btnPrev = document.querySelector('.js-slider-btn-prev');
+  const btnNext = document.querySelector('.js-slider-btn-next');
+  const spanPrev = document.querySelector('.js-span-prev');
+  const spanNext = document.querySelector('.js-span-next');
 
+  let fullCount = images.length;
   let count = 0;
   let width;
 
-  function init() {
+  function calculateValue() {
     width = document.querySelector('.js-slider-track').offsetWidth;
     sliderLine.style.width = width * images.length + 'px';
-
     images.forEach(item => (item.style.width = width + 'px'));
     rollSlider();
   }
+  window.addEventListener('resize', calculateValue);
+  calculateValue();
 
-  window.addEventListener('resize', init);
-  init();
-
-  document.querySelector('.js-slider-btn-next').addEventListener('click', () => {
-    count += 1;
-    if (count >= images.length) count = 0;
-    rollSlider();
-  });
-
-  document.querySelector('.js-slider-btn-prev').addEventListener('click', () => {
+  btnPrev.addEventListener('click', e => {
     count -= 1;
     if (count < 0) count = images.length - 1;
     rollSlider();
   });
 
+  btnNext.addEventListener('click', e => {
+    count += 1;
+    if (count >= images.length) count = 0;
+    rollSlider();
+  });
+
   function rollSlider() {
     sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    spanPrev.textContent = count + 1;
+    spanNext.textContent = fullCount - count;
+    spanPrev.textContent = spanPrev.textContent.padStart(2, '0');
+    spanNext.textContent = spanNext.textContent.padStart(2, '0');
   }
 }
