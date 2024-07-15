96fe12d7c1549d110e1102d84774b9aa2ed29ac7
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-15 14:00:47 +0300

    add

--- src/js/partials/hamburger-btn.js
+++ src/js/partials/hamburger-btn.js
@@ -1,11 +1,20 @@
 export function hamburgerMenu() {
-  let hamburger = document.querySelector('#hamburger');
+  const hamburger = document.querySelector('#hamburger');
+  const mobMenu = document.querySelector('.js-menu');
 
-  hamburger.addEventListener('click', animationHamburger);
+  hamburger.addEventListener('click', e => {
+    animationHamburgerBtn();
+    toggleMobMenu();
+  });
 
-  function animationHamburger() {
+  function animationHamburgerBtn() {
     [...hamburger.children].forEach(el => {
       el.classList.toggle('is-open');
     });
+  }
+
+  function toggleMobMenu() {
+    mobMenu.classList.toggle('mob-menu_visible');
+    document.body.classList.toggle('no-scroll');
   }
 }
