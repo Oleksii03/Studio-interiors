ebcb672335cddd6d921bfa2c0badf7e410db0568
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-18 17:53:26 +0300

    add

--- src/js/partials/hamburger-btn.js
+++ src/js/partials/hamburger-btn.js
@@ -5,16 +5,36 @@ export function hamburgerMenu() {
   hamburger.addEventListener('click', e => {
     animationHamburgerBtn();
     toggleMobMenu();
+    invertColors();
+    // if (window.location.pathname === '/about.html') {
+    // }
   });
 
   function animationHamburgerBtn() {
     [...hamburger.children].forEach(el => {
-      el.classList.toggle('is-open');
+      if (!el.classList.contains('is-open')) {
+        el.classList.add('is-open');
+      } else {
+        el.classList.remove('is-open');
+        el.classList.add('is-closed');
+      }
     });
   }
 
   function toggleMobMenu() {
     mobMenu.classList.toggle('mob-menu_visible');
     document.body.classList.toggle('no-scroll');
+  }
+
+  function invertColors() {
+    const logo = document.querySelector('[data-logo="logo"]');
+    const menuText = document.querySelector('[data-menu="text-menu"]');
+    const menuLines = document.querySelectorAll('[data-line="line"]');
+    const leng = document.querySelector('[data-leng="leng"]');
+    const btnSend = document.querySelector('[data-send-btn="send-btn"]');
+
+    [logo, menuText, ...menuLines, leng, btnSend].forEach(el => {
+      el.classList.toggle('invert');
+    });
   }
 }
