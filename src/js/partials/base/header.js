ffc8725dbb2a35bb831efee51ac9799d25750718
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-07-22 20:44:22 +0300

    add

--- src/js/partials/base/header.js
+++ src/js/partials/base/header.js
@@ -3,52 +3,63 @@ import debounce from 'lodash.debounce';
 export function header() {
   const header = document.querySelector('.js-header');
 
-  const options = {
-    leading: true,
-    trailing: true,
-  };
-
-  const debouncedActiveHeader = debounce(activeHeader, 250, options);
-
-  window.addEventListener('scroll', debouncedActiveHeader);
+  window.addEventListener('scroll', debounce(activeHeader, 250));
 
   function activeHeader() {
-    const activeScroll = window.scrollY > 60;
-    const activeHeader = header.classList.contains('header_active');
-    const opacityHeader = header.classList.contains('opacity');
-
     if (window.scrollY < 60) {
       header.classList.remove('header_active');
-      header.classList.remove('opacity');
-      header.style.opacity = 1;
-    } else if (activeScroll && !activeHeader) {
-      header.classList.add('header_active');
-    } else if (activeScroll && activeHeader) {
-      header.classList.remove('header_active');
+      return;
     }
 
-    if (!opacityHeader && activeScroll) {
-      header.style.opacity = 1;
-      header.classList.add('opacity');
-    } else if (activeScroll && opacityHeader) {
-      header.style.opacity = 0;
-      header.classList.remove('opacity');
-    }
+    header.classList.add('header_active');
   }
 
-  window.addEventListener('resize', onReload);
-  window.addEventListener('load', onReload);
-  function onReload() {
-    setTimeout(() => {
-      if (
-        window.scrollY > 60 &&
-        !header.classList.contains('header_active') &&
-        !header.classList.contains('opacity')
-      ) {
-        header.classList.add('header_active');
-        header.style.opacity = 1;
-        header.classList.add('opacity');
-      }
-    }, 400);
-  }
+  // const options = {
+  //   leading: true,
+  //   trailing: true,
+  // };
+
+  // const debouncedActiveHeader = debounce(activeHeader, 250, options);
+
+  // window.addEventListener('scroll', debouncedActiveHeader);
+
+  // function activeHeader() {
+  //   const activeScroll = window.scrollY > 60;
+  //   const activeHeader = header.classList.contains('header_active');
+  //   const opacityHeader = header.classList.contains('opacity');
+
+  //   if (window.scrollY < 60) {
+  //     header.classList.remove('header_active');
+  //     header.classList.remove('opacity');
+  //     header.style.opacity = 1;
+  //   } else if (activeScroll && !activeHeader) {
+  //     header.classList.add('header_active');
+  //   } else if (activeScroll && activeHeader) {
+  //     header.classList.remove('header_active');
+  //   }
+
+  //   if (!opacityHeader && activeScroll) {
+  //     header.style.opacity = 1;
+  //     header.classList.add('opacity');
+  //   } else if (activeScroll && opacityHeader) {
+  //     header.style.opacity = 0;
+  //     header.classList.remove('opacity');
+  //   }
+  // }
+
+  // window.addEventListener('resize', onReload);
+  // window.addEventListener('load', onReload);
+  // function onReload() {
+  //   setTimeout(() => {
+  //     if (
+  //       window.scrollY > 60 &&
+  //       !header.classList.contains('header_active') &&
+  //       !header.classList.contains('opacity')
+  //     ) {
+  //       header.classList.add('header_active');
+  //       header.style.opacity = 1;
+  //       header.classList.add('opacity');
+  //     }
+  //   }, 400);
+  // }
 }
